a7442ab6014dfca2bd9849fb2f66f5b4be0e5aed
Tim Yusupov <timboyus@gmail.com>
2023-12-16 20:43:51 +0300

    feat: add welcome page

--- src/components/pages/Welcome/Welcome.tsx
+++ src/components/pages/Welcome/Welcome.tsx
@@ -1,9 +1,51 @@
-import { useTranslation } from '../../../hooks';
-import AnimatedInner from '../../shared/AnimatedInner/AnimatedInner.tsx';
+import { useAppSelector, useTranslation } from '../../../hooks';
+import AnimatedInner from '../../shared/AnimatedInner/AnimatedInner';
+import type { HeaderButton } from '../../../types';
+import { selectAuth } from '../../../store/slices/userSlice';
+import NavigationButton from '../../shared/NavigationButton/NavigationButton';
+
+import styles from './Welcome.module.css';
 
 const Welcome = () => {
   const translation = useTranslation();
-  return <AnimatedInner inner={translation.welcome} />;
+  const { isAuthenticated } = useAppSelector(selectAuth);
+
+  const buttons: HeaderButton[] = isAuthenticated
+    ? [
+        {
+          value: 'GraphiQL',
+          to: '/main',
+        },
+      ]
+    : [
+        {
+          value: translation.signin,
+          to: '/sign-in',
+        },
+        {
+          value: translation.signup,
+          to: '/sign-up',
+        },
+      ];
+
+  return (
+    <div className={styles.main}>
+      <AnimatedInner inner={translation.welcome} />
+      <div className={styles.sectionInfo}>
+        {translation.projectInfo.map((name, index) => (
+          <p className={styles.text} key={index}>
+            {name}
+          </p>
+        ))}
+      </div>
+      <div className={styles.buttonsContainer}>
+        {' '}
+        {buttons.map(({ value, to, func }) => (
+          <NavigationButton key={value} value={value} to={to} func={func} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Welcome;
